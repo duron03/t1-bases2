f66b8fb58d79bff5fd3c5d713605edc4a73a708e
duron03 <noelopes100@gmail.com>
2026-09-06 19:29:33 -0600

    Inicialización del proyecto
    Se inicializa el directorio de archivos con los archivos principales de configuración para el desarrollo de la API REST.

---
+++ codigo/src/config.js
@@ -0,0 +1,11 @@
+import { config } from 'dotenv';
+
+config();
+
+export const PORT = process.env.PORT;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_SERVER = process.env.DB_SERVER;
+export const DB_DATABASE = process.env.DB_DATABASE;
+
+console.log(DB_DATABASE);
